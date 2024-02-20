bed6b6236b84463bae5dee91f15d3ff0f1767b2f
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-20 19:20:05 +0330

    Chore: Media files Configuration

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,11 @@ STATICFILES_DIRS = [
 ]
 
 
+# Media files(Uploaded files from user)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
