60986a14b62e322bbf55b71c8631d46e674ba45e
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-20 19:25:55 +0330

    Fix: AccountConfig --> AccountsConfig

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
-    'accounts.apps.AccountConfig',
+    'accounts.apps.AccountsConfig',
 
 ]
 
