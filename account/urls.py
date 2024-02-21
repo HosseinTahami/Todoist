90ba9b81b328312a0d5db9564eac485c2b86dc23
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-21 18:49:11 +0330

    Chore: urls files for django apps

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+# Django Imports
+
+from django.urls import path
+
+# Inside Project Imports
+
+from . import views
+
+
+app_name = 'account'
+
+urlpatterns = [
+
+]
---
+++ task/urls.py
@@ -0,0 +1,15 @@
+# Django Imports
+
+from django.urls import path
+
+# Inside Project Imports
+
+from . import views
+
+
+app_name = 'task'
+
+
+urlpatterns = [
+
+]
