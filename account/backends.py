c3301a522ed4df595e9cb01504ac8a2344162988
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-29 12:35:00 +0330

    Feat: Add UsernameBackend

--- account/backends.py
+++ account/backends.py
@@ -11,7 +11,25 @@ class EmailBackend(BaseBackend):
     def authenticate(self, request, email, password):
         try:
             user = User.objects.get(email=email)
-            if user.check_password(password=password):
+            if user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
+
+class UsernameBackend(BaseBackend):
+
+    def authenticate(self, request, username, password):
+        try:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
                 return user
             return None
         except User.DoesNotExist:
