4878d31fcf0deeb43dd71dc6002ff82f675baef4
HosseinTahami <hosseintahami2000@gmail.com>
2024-02-29 12:10:55 +0330

    Chore: AUTH_USER_MODEL

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     }
 }
 
+# Custom User
+# https://docs.djangoproject.com/en/5.0/ref/settings/#std-setting-AUTH_USER_MODEL
+AUTH_USER_MODEL = "account.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
