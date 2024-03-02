4b47bd1a1886c1b1cb1716a996653adf8d7360ca
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-02 06:17:25 +0330

    Feat: Add Task Model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,25 @@
+# Django Imports
 from django.db import models
 
-# Create your models here.
+
+# Inside Project Imports
+from account.models import User
+
+
+class Task(models.Model):
+    PRIORITY_CRITICAL = "C"
+    PRIORITY_MEDIUM = "M"
+    PRIORITY_LOW = "L"
+    PRIORITY_CHOICES = [
+        (PRIORITY_CRITICAL, "Critical"),
+        (PRIORITY_MEDIUM, "Medium"),
+        (PRIORITY_LOW, "Low")
+    ]
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=70)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    priority = models.CharField(
+        max_length=1, choices=PRIORITY_CHOICES, default=PRIORITY_LOW)
