561c7656830ece52f4781a5b54cf0f7837b28bc7
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-02 09:55:33 +0330

    Chore: Add category and task urls

--- task/urls.py
+++ task/urls.py
@@ -7,4 +7,6 @@ from . import views
 app_name = 'task'
 
 urlpatterns = [
+    path('<int:pk>/tasks/', views.TaskView.as_view(), name='tasks'),
+    path('<int:pk>/categories/', views.CategoryView.as_view(), name='categories'),
 ]
