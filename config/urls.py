aa17143592a964bf4695271b2524dfffa71655e3
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-02 06:42:17 +0330

    Chore: Add task.urls to urls.py

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls', namespace='account')),
     path('', include('core.urls', namespace='core')),
-    path('task', include('task.urls', namespace='task')),
+    path('task/', include('task.urls', namespace='task')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
