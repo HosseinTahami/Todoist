b10caad2aa14323d38eb53674ca94f75f7d55f00
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-02 09:54:49 +0330

    Feat: Add Category & Task View Class

--- task/views.py
+++ task/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# Django Imports
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Inside Project Imports
+
+from .models import Category, Task
+
+
+class TaskView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        tasks = Task.objects.all().filter(user=user)
+        return render(request, 'task/tasks.html', {'tasks': tasks})
+
+
+class CategoryView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        categories = Category.objects.all().filter(user=user)
+        return render(request, 'task/categories.html', {'categories': categories})
