9118f2806aebb68ca392b16978e055c3d1dd89c2
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-02 22:47:33 +0330

    Chore: Add login & logout urls

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,8 @@ from . import views
 app_name = "account"
 
 urlpatterns = [
-    path('profile/<int:pk>/', views.ProfileView.as_view(), name='user_profile'),
-    path('register/', views.UserRegisterView.as_view(), name='login'),
+    path('profile/<int:pk>/', views.ProfileView.as_view(), name='profile'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
 ]
