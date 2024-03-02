f09a936e62fec2d2aebcc38f6cea2bce6020f77b
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-02 21:30:34 +0330

    Feat: Add UserLoginForm

--- account/forms.py
+++ account/forms.py
@@ -5,9 +5,23 @@ from django import forms
 from . import models
 
 
-class ProfileViewForm(forms.Form):
+class ProfileViewForm(forms.ModelForm):
 
     class Meta:
         model = models.User
         fields = ['email', 'first_name',
                   'last_name', 'username', 'date_of_birth']
+
+
+class UserLoginForm(forms.ModelForm):
+
+    confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': "*****"}))
+
+    class Meta:
+        model = models.User
+        fields = ['email', 'password', 'confirm_password']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'mail@email.com'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': '*****'})
+        }
