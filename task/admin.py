a879c630a4829601833c7253138fea0aa6f8c6a6
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-04 18:30:52 +0330

    Perf: Better Register Category Model

--- task/admin.py
+++ task/admin.py
@@ -6,14 +6,22 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Category)
-
 
 @admin.register(models.Task)
 class TaskAdmin(admin.ModelAdmin):
 
-    list_display = ['title', 'user', 'created_at', 'priority']
+    list_display = ['id', 'title', 'user', 'created_at', 'priority']
     list_editable = ['priority']
     list_per_page = 10
     list_filter = ['priority']
     ordering = ['user']
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'title', 'user', 'created_at']
+    list_editable = ['user']
+    list_filter = ['user']
+    ordering = ['user']
+    search_fields = ['user']
