eae2fca15b2f548b21a07c5ff6c857b710000cca
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-04 09:51:52 +0330

    Feat: Add Update & Detail View for task

--- task/urls.py
+++ task/urls.py
@@ -9,5 +9,6 @@ app_name = 'task'
 urlpatterns = [
     path('<int:pk>/tasks/', views.TaskView.as_view(), name='tasks'),
     path('<int:pk>/categories/', views.CategoryView.as_view(), name='categories'),
-    path('task/<int:pk>/', views.UpdateTaskView.as_view(), name='update_task'),
+    path('task/update/<int:pk>/', views.UpdateTaskView.as_view(), name='update_task'),
+    path('task/detail/<int:pk>/', views.DetailTaskView.as_view(), name='detail_task'),
 ]
