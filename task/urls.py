4a40a1c6197a6ddb1829a33d97d4524c1e073535
HosseinTahami <hosseintahami2000@gmail.com>
2024-03-04 11:03:33 +0330

    Feat: Add detail & update category urls

--- task/urls.py
+++ task/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path('<int:pk>/categories/', views.CategoryView.as_view(), name='categories'),
     path('task/update/<int:pk>/', views.UpdateTaskView.as_view(), name='update_task'),
     path('task/detail/<int:pk>/', views.DetailTaskView.as_view(), name='detail_task'),
+    path('category/update/<int:pk>/',
+         views.UpdateCategoryView.as_view(), name='update_category'),
+    path('category/detail/<int:pk>/',
+         views.DetailCategoryView.as_view(), name='detail_category'),
+
 ]
